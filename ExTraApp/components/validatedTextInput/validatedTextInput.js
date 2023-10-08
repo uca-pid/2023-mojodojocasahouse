4795bb1fecf61409ae5ba4151e59a80d4da77761
tomasagata <tomasagata00@gmail.com>
2023-10-08 12:28:19 -0300

    Code documentation for ValidatedTextInput component

--- ExTraApp/components/validatedTextInput/validatedTextInput.js
+++ ExTraApp/components/validatedTextInput/validatedTextInput.js
@@ -3,11 +3,16 @@ import { View } from 'react-native';
 import { TextInput, HelperText } from 'react-native-paper';
 import styles from './style';
 
-/*
-  Takes three props: label, value, onChangeText, hasErrorCallback, validationErrorMessage.
-
-  Once user has finished editing input, an error check is done, calling hasErrorCallback(input).
-  If an error is found, validationErrorMessage is displayed.
+/** 
+ * Takes three props: ```label```, ```value```, ```onChangeText```, ```hasErrorCallback```, ```validationErrorMessage```.
+ * Once user has finished editing input, an error check is done, calling hasErrorCallback(input).
+ * If an error is found, validationErrorMessage is displayed.
+ * 
+ * @param {string} label Shown label
+ * @param {string} value React state variable that holds field value
+ * @param {string} validationErrorMessage Error message to show user if field has error
+ * @param {function} onChangeText React state callback to update React state variable value
+ * @param {function} hasErrorCallback Custom callback to detect if field has undesired errors
 */
 const ValidatedTextInput = props => {
 
