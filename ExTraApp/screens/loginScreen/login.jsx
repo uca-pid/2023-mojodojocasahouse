26cafa254320e2ee484c6e1cbcb4596231467c59
Pedro Ortiz de Guinea <66389082+peportizdg@users.noreply.github.com>
2023-10-21 12:26:27 -0300

    added eye function but not visible

--- ExTraApp/screens/loginScreen/login.jsx
+++ ExTraApp/screens/loginScreen/login.jsx
@@ -9,6 +9,7 @@ import LoadingOverlay from '../../components/loading/loading';
 const Login = ({ navigation, route }) => { 
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [secureTextEntry, setSecureTextEntry] = React.useState(true);
   const [loading, setLoading] = React.useState(false);
   const [rememberMe, setRememberMe] = React.useState(false);
 
@@ -42,7 +43,7 @@ const Login = ({ navigation, route }) => {
 
         <View style={styles.bottomContainer}></View>
 
-        <ScrollView contentContainerStyle={styles.contentContainer}>
+        <ScrollView contentContainerStyle={styles.contentContainer} keyboardShouldPersistTaps="handled">
         
           <TextInput
             style={{ marginLeft: '10%', width: '80%', marginBottom: '5%' }}
@@ -53,12 +54,13 @@ const Login = ({ navigation, route }) => {
           />
 
           <TextInput
-            secureTextEntry={true}
+            secureTextEntry={secureTextEntry}
             style={{ marginLeft: '10%', width: '80%', marginBottom: '5%' }}
             label="Password"
             value={password}
             onChangeText={password => setPassword(password)}
             maxLength={100}
+            right={<TextInput.Icon name= 'eye' onPress={() => setSecureTextEntry(!secureTextEntry)} />}
           />
           <Text style={styles.rememberMeText} >Remember me:</Text>
           <Text style={styles.rememberMeBox}><Switch value={rememberMe} onValueChange={onToggleSwitch} /></Text>
