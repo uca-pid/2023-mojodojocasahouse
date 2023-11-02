cf5b15fb6a63e57d3be2211d031894b4f52a7ff7
tomasagata <tomasagata00@gmail.com>
2023-11-02 15:22:08 -0300

    Removed unused styles in table screen

--- src/screens/TableScreen.jsx
+++ src/screens/TableScreen.jsx
@@ -7,7 +7,7 @@ import { Dialog, ListItem, Button, Icon as MaterialIcon } from '@rneui/themed';
 import { AuthContext } from '../context/AuthContext';
 import FilterModal from '../components/FilterModal';
 import EditExpenseModal from '../components/EditExpenseModal';
-import {LinearGradient} from 'react-native-linear-gradient';
+import { LinearGradient } from 'react-native-linear-gradient';
 
 
 
@@ -236,8 +236,6 @@ const styles = StyleSheet.create({
     width: '96%',
   },
 
-
-
   // Logo Container
 
   headerContainer: {
@@ -267,72 +265,6 @@ const styles = StyleSheet.create({
     aspectRatio: 2,
     resizeMode: 'contain',
   },
-
-  logoutButtonContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    height: '100%',
-    aspectRatio: 0.7,
-
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  logoutButton: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    borderRadius: 10,
-    backgroundColor: '#d15c54',
-    height: '70%',
-    aspectRatio: 0.7,
-    borderWidth: 1.5,
-    borderColor: "#d15c54",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-
-    elevation: 5,
-
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  logoutButtonIcon: {
-    fontSize: 23,
-  },
-
-
-
-
-  // Menu button container
-
-  menuContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    width: '100%',
-    marginTop: 20,
-
-    justifyContent: 'space-evenly',
-  },
-
-  menuItemContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    width: 100,
-  },
-
-
-
-
-
 
   // Bottom container
 
@@ -376,11 +308,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 
-
-
-
-
-
   // Scrollview
 
   scrollviewContentContainer: {
@@ -391,161 +318,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#AEB4E7', // Border color
     borderRadius: 5,
-  },
-
-  tableContainer: {
-    display: 'flex',
-    margin: 16,
-    backgroundColor: '#e1e1e8', // Table background color
-    borderWidth: 1,
-    borderColor: '#AEB4E7', // Border color
-    borderRadius: 5,
-  },
-
-  
-  row: {
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%',
-    height: 40,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-
-  iconContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    height: '100%',
-    aspectRatio: 1,
-    
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  icon: {
-    display: 'flex',
-    flexDirection: 'row',
-    color: 'black',
-
-    fontSize: 25,
-  },
-
-  rowMiddleContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-
-    width: 200, // Máximo posible
-    height: '100%',
-
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
-  },
-
-  conceptContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    width: '100%',
-    height: '50%',
-
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
-
-  concept: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    fontSize: 15,
-    color: '#000000',
-
-    textAlign: 'left',
-  },
-
-  categoryContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    width: '100%',
-    height: '50%',
-
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
-
-  categoryModalContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    marginTop: '50%',
-    marginLeft: '10%',
-    marginRight: '10%',
-    backgroundColor: '#ADC4C0',
-    borderRadius: 10,
-    padding: 20,
-  },
-
-  category: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    fontSize: 13,
-    color: '#5c5c5c',
-
-    textAlign: 'left',
-  },
-
-  rowLeftContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-
-    width: 80,
-    height: '100%',
-
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
-  },
-
-  amountContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    width: '100%',
-    height: '50%',
-
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-
-  amount: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    fontSize: 15,
-    color: '#000000',
-
-    textAlign: 'left',
-  },
-
-  dateContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    width: '100%',
-    height: '50%',
-
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-
-  date: {
-    display: 'flex',
-    flexDirection: 'row',
-
-    fontSize: 11,
-    color: '#5c5c5c',
-
-    textAlign: 'left',
   },
 
 });
