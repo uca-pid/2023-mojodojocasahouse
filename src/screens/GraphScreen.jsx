e6f6e3baf4c19ab98dd4d72f9750e8509d6ede8f
Tomas Agata <89418101+tomasagata@users.noreply.github.com>
2023-11-15 17:03:46 -0300

    Feature: New Pie-Chart Legend UI (#46)
    * BarCharts Can now be filtered
    
    * Added new legend to piechart
    
    ---------
    
    Co-authored-by: camilaDiMontezemolo <camiladimon@hotmail.com>

--- src/screens/GraphScreen.jsx
+++ src/screens/GraphScreen.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Image, Alert, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, TouchableOpacity, Alert, FlatList, StyleSheet } from 'react-native';
 import { PieChart } from "react-native-chart-kit";
-import { fetchUserCategories,fetchExpensesList} from '../utils/apiFetch';
+import { useNavigation } from '@react-navigation/native';
+import { ListItem } from 'react-native-elements';
+import { fetchUserCategories, fetchExpensesList } from '../utils/apiFetch';
 import { AuthContext } from '../context/AuthContext';
 import FilterModal from '../components/FilterModal';
 import ScreenTemplate from '../components/ScreenTemplate';
@@ -11,11 +12,12 @@ const GraphScreen = () => {
   const [loading, setLoading] = React.useState(false);
   const [expenses, setExpenses] = React.useState([]);
   const [categories, setCategories] = React.useState([]);
+  const [categoryExpenses, setCategoryExpenses] = React.useState([])
   const [isFilterModalVisible, setFilterModalVisible] = React.useState(false);
   const [legendData, setLegendData] = React.useState([]);
-  const {signOut, sessionExpired} = React.useContext(AuthContext);
+  const { signOut, sessionExpired } = React.useContext(AuthContext);
 
-  
+
 
 
   const handleFocusScreen = async () => {
@@ -23,32 +25,32 @@ const GraphScreen = () => {
     await fetchExpensesList(setExpenses, sessionExpired);
     setLoading(false);
   };
-    
-  
+
+
   const randomColorGenerator = (idx) => {
-    const sliceColor = [      
-    '#DF928E',
-    '#47132D',
-    '#CC0066',
-    '#A24875',
-    '#FF5CAD',
-    '#D47DA8',
-    '#DCBACB',
-    '#634F59',
-    '#EE4097',
-    '#F82A91',
-    '#210010',
-    '#CE5650',
-    '#F8ACA8',
-    '#FA9791',
-    '#EE7A74',
-    '#EE665F',
-    '#B43831',
-    '#DC4037',
-    '#894542',
-    '#744340',
-    '#B47E7B',
-    '#DE9894'];
+    const sliceColor = [
+      '#DF928E',
+      '#47132D',
+      '#CC0066',
+      '#A24875',
+      '#FF5CAD',
+      '#D47DA8',
+      '#DCBACB',
+      '#634F59',
+      '#EE4097',
+      '#F82A91',
+      '#210010',
+      '#CE5650',
+      '#F8ACA8',
+      '#FA9791',
+      '#EE7A74',
+      '#EE665F',
+      '#B43831',
+      '#DC4037',
+      '#894542',
+      '#744340',
+      '#B47E7B',
+      '#DE9894'];
     return sliceColor[idx];
   }
 
@@ -59,7 +61,7 @@ const GraphScreen = () => {
   const handleFilterModalSubmit = async (data) => {
     setLoading(true);
     await fetchExpensesList(setExpenses, sessionExpired, data);
-  
+
     // Calculate category expenses and update the legend
     const updatedCategoryExpenses = calculateCategoryExpenses();
     const updatedLegendData = Object.keys(updatedCategoryExpenses).map((category, idx) => ({
@@ -67,7 +69,7 @@ const GraphScreen = () => {
       population: updatedCategoryExpenses[category],
       color: randomColorGenerator(idx),
     }));
-  
+
     setLegendData(updatedLegendData);
     setFilterModalVisible(false);
     setLoading(false);
@@ -78,7 +80,7 @@ const GraphScreen = () => {
     backgroundGradientFromOpacity: 0,
     backgroundGradientTo: "#47132D",
     backgroundGradientToOpacity: 0.5,
-    color: () => `rgb(${Math.floor(Math.random()*255)}, 255, 146)`,
+    color: () => `rgb(${Math.floor(Math.random() * 255)}, 255, 146)`,
     strokeWidth: 2, // optional, default 3
     barPercentage: 0.5,
     useShadowColorFromDataset: false // optional
@@ -96,14 +98,17 @@ const GraphScreen = () => {
       }
       categoryExpenses[expense.category] += expense.amount;
     });
+    console.log("categoryExnses:", categoryExpenses)
     return categoryExpenses;
   };
-    
+
   React.useEffect(() => {
-    const unsubscribe = navigation.addListener("focus", () => {
-      try{
+    const unsubscribe = navigation.addListener("focus", async () => {
+      try {
+
         setLoading(true);
-        handleFocusScreen();
+        await handleFocusScreen();
+
       } catch (error) {
         setLoading(false);
         Alert.alert("Connection Error", "There was an error connecting to API");
@@ -112,39 +117,76 @@ const GraphScreen = () => {
 
     return unsubscribe;
   }, [navigation]);
-  
-  const categoryExpenses = calculateCategoryExpenses();
-  
+
+  React.useEffect(() => {
+    if(expenses.length){
+      const updatedCategoryExpenses = calculateCategoryExpenses();
+      const updatedLegendData = Object.keys(updatedCategoryExpenses).map((category, idx) => ({
+        name: category,
+        population: updatedCategoryExpenses[category],
+        color: randomColorGenerator(idx),
+      }));
+
+      setLegendData(updatedLegendData);
+      setCategoryExpenses(updatedCategoryExpenses)
+      setLoading(false);
+    }
+  }, [expenses])
+
+
+  // const categoryExpenses = calculateCategoryExpenses();
+
+  const renderItem = ({ item }) => (
+    <ListItem  key={item} bottomDivider>
+      <ListItem.Content style={styles.listItemContent}>
+        <View style={{ backgroundColor: item.color, width: 20, height: 20, marginRight: 10 }} />
+        <View style={styles.listItemTextContainer}>
+          <ListItem.Title style={styles.listItemTitle}>{item.name}</ListItem.Title>
+          <ListItem.Subtitle style={{ color: 'gray', marginLeft: 10 }} >${item.population.toFixed(2)}</ListItem.Subtitle>
+        </View>
+      </ListItem.Content>
+    </ListItem>
+  );
+
   return (
     <ScreenTemplate loading={loading}>
       <ScreenTemplate.Logo />
 
       <ScreenTemplate.Content>
         <View style={styles.addExpenseButtonContainer}>
-        <TouchableOpacity style={styles.button} onPress={toggleFilterModal}>
+          <TouchableOpacity style={styles.button} onPress={toggleFilterModal}>
             <Text style={styles.buttonText}>Choose Categories and Date Range</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.e}>
           {Object.keys(categoryExpenses).length ? (
-            <PieChart
-            data={Object.keys(categoryExpenses).map((category, idx) => ({
-              name: category,
-              population: categoryExpenses[category],
-              color: randomColorGenerator(idx),
-            }))}
-            width={340}
-            height={200}
-            chartConfig={chartConfig}
-            accessor="population"
-            backgroundColor="transparent"
-            paddingLeft="15"
-            absolute
-            legendData={legendData} // Pass the legend data here
-          />
+            <>
+              <PieChart
+                data={Object.keys(categoryExpenses).map((category, idx) => ({
+                  name: category,
+                  population: categoryExpenses[category],
+                  color: randomColorGenerator(idx),
+                }))}
+                width={340}
+                height={200}
+                chartConfig={chartConfig}
+                accessor="population"
+                backgroundColor="transparent"
+                paddingLeft="90"
+                absolute
+                hasLegend={false}
+              />
+              <FlatList
+                data={legendData}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.name}
+              />
+            </>
           ) : (
-            <Text style={styles.textForNoData}>There is no expenses data available for selected parameters. Please try again</Text>
+            <Text style={styles.textForNoData}>
+              There is no expenses data available for selected parameters. Please try again
+            </Text>
           )}
         </View>
       </ScreenTemplate.Content>
@@ -157,7 +199,7 @@ const GraphScreen = () => {
 const styles = StyleSheet.create({
 
   addExpenseButtonContainer: {
-    height: '12%',
+    height: '8%',
     width: '80%',
     marginLeft: '10%',
     marginTop: '1%',
@@ -177,6 +219,25 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: 'bold',
   },
+
+  listItemContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 10,
+    color: 'black',
+    position: 'absolute'
+  },
+  listItemTextContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    color: 'black',
+  },
+  listItemTitle: {
+    marginLeft: 10,
+    color: 'black',
+
+  },
+
 
   // Scrollview
 
